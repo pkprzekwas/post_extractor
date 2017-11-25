f7409ab2a22e7533ec2b370065a943e1cee093e6
Patryk Przekwas <pkprzekwas@gmail.com>
2017-11-25 16:42:59 +0100

    fixed name convention

--- modules/posts.py
+++ modules/posts.py
@@ -13,37 +13,37 @@ from pyspark.sql.types import ArrayType, StringType
 class PostTransformer(Transformer, HasInputCol, HasOutputCol):
     def __init__(self):
         super().__init__()
-        
+
     def _transform(self, dataframe):
         out_col = self.getOutputCol()
         in_col = self.getInputCol()
-        
+
         def get_content(data):
             contents = [doc.get('content') for doc in data]
             return contents
-        
+
         get_cntn = udf(get_content, ArrayType(StringType()))
         return dataframe.withColumn(out_col, get_cntn(in_col))
-  
+
 
 class TranslateTransformer(Transformer, HasInputCol, HasOutputCol):
     def __init__(self):
         super().__init__()
-        
+
     def _transform(self, dataframe):
         out_col = self.getOutputCol()
         in_col = self.getInputCol()
-        
+
         def translate(p):
             try:
                 return str(TextBlob(p).translate(from_lang='pl'))
             except (NotTranslated, TranslatorError) as e:
                 return 'Translation error'
-        
+
         def process(data):
             translated = [translate(p) for p in data]
             return translated
-        
+
         process = udf(process, ArrayType(StringType()))
         return dataframe.withColumn(out_col, process(in_col))
 
@@ -51,17 +51,17 @@ class TranslateTransformer(Transformer, HasInputCol, HasOutputCol):
 class SentenceTransformer(Transformer, HasInputCol, HasOutputCol):
     def __init__(self):
         super().__init__()
-        
+
     def _transform(self, dataframe):
         out_col = self.getOutputCol()
         in_col = self.getInputCol()
-        
-        def get_content(data):
+
+        def extract_sentences(data):
             sentences = []
             for post in data:
                 for sentence in TextBlob(post).sentences:
                     sentences.append(str(sentence))
             return sentences
-        
-        get_cntn = udf(get_content, ArrayType(StringType()))
-        return dataframe.withColumn(out_col, get_cntn(in_col))
+
+        ext_sentn = udf(extract_sentences, ArrayType(StringType()))
+        return dataframe.withColumn(out_col, ext_sentn(in_col))
